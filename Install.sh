8e1cbfeb8edc6a23039c5f937a3aa41c88e36686
techtricks1 <69209262+techtricks1@users.noreply.github.com>
2020-08-08 10:48:37 +0530

    Create Install.sh

---
+++ Install.sh
@@ -0,0 +1,18 @@
+command_not_found_handle() {
+        /data/data/com.termux/files/usr/libexec/termux/command-not-found "$1"
+}
+
+PS1='\[\e[31m\]┌─[\[\e[37m\]\T\[\e[31m\]]─────\e[1;93m[eshwar]\e[0;31m───[\#]\n|\n\e[0;31m└─[\[\e[31m\]\e[0;35m\W\[\e[31m\]]────►\e[1;92m'
+
+clear
+echo -e "Created By \e[5mtechtricks1"
+echo "----------------------------" | lolcat
+echo -e "\e[1mInstagram :@techtricks"
+echo -e "\e[1mGithub: github.com/techtricks1/"
+echo "----------------------------" | lolcat
+echo "
++-+-+-+-+-+-+-+
+|W|e|l|c|o|m|e|
++-+-+-+-+-+-+-+ " | lolcat
+date | lolcat
+echo ""
